4b17cc38f684f9099e9262eff697dc3e44b8a18c
pesho <peter12345@abv.bg>
2018-05-01 00:06:26 +0300

    index.js updated

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,7 @@
 const homeHandler = require('./home')
 const filesHandler = require('./static-files')
 const productHandler = require('./product')
+const categoryHandler = require('./category')
 const errorHandler = require('./error')
 
-module.exports = [homeHandler, productHandler, filesHandler, errorHandler]
+module.exports = [homeHandler, productHandler, filesHandler, categoryHandler, errorHandler]
