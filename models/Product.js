b1484df05d44fe06ad9d658379550c63d1f290a5
pesho <peter12345@abv.bg>
2018-04-30 19:42:42 +0300

    Product model created

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+let productSchema = mongoose.Schema({
+  name: {type: 'String', required: true},
+  description: {type: 'String'},
+  price: {
+    type: 'Number',
+    min: 0,
+    max: Number.MAX_VALUE,
+    default: 0
+  },
+  image: {type: 'String'},
+  isBought: {type: 'Boolean', default: false},
+  category: {type: 'ObjectId', ref: 'Category'}
+})
+
+let Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
