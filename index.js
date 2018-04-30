890310d0e74ae72109c5659351f6e4e21ae530fe
pesho <peter12345@abv.bg>
2018-04-30 19:43:52 +0300

    index.js updated

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const http = require('http')
 const port = 3000
 const handlers = require('./handlers/index')
 
+let environment = process.env.NODE_ENV || 'development'
+const config = require('./config/config')
+const database = require('./config/database.config')
+
+database(config[environment])
+
 http.createServer((req, res) => {
   for (const handler of handlers) {
     let result = handler(req, res)
