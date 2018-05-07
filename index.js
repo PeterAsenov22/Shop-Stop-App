7da955d3af66b7f9449ddd5adf90c2780ca9f947
pesho <peter12345@abv.bg>
2018-05-07 18:56:36 +0300

    index.js updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let app = express()
 database(config[environment])
 require('./config/express')(app, config[environment])
 require('./config/routes')(app)
+require('./config/passport')()
 
 app.listen(port)
 console.log(`Server listening on port ${port}...`)
