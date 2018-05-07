5bbdd4b1639e337bd5fb06955b0199aa1f73229f
pesho <peter12345@abv.bg>
2018-05-07 18:54:28 +0300

    express configuration updated

--- config/express.js
+++ config/express.js
@@ -2,11 +2,19 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
 
 module.exports = (app, config) => {
   app.engine('handlebars', exphbs({defaultLayout: 'main'}))
   app.set('view engine', 'handlebars')
+
   app.use(bodyParser.urlencoded({extended: true}))
+  app.use(cookieParser())
+  app.use(session({secret: 'S3cr3t', saveUninitialized: false, resave: false}))
+  app.use(passport.initialize())
+  app.use(passport.session())
 
   app.use((req, res, next) => {
     if (req.url.startsWith('/content')) {
