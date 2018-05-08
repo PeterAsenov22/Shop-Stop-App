5bb5ed5ebed1611b591c9ef95da59e237396c4b4
pesho <peter12345@abv.bg>
2018-05-08 16:53:45 +0300

    authorization added

--- config/routes.js
+++ config/routes.js
@@ -1,25 +1,27 @@
 const handlers = require('../controllers/index')
 const multer = require('multer')
 let upload = multer({dest: './content/images'})
+const auth = require('./auth')
 
 module.exports = (app) => {
   app.get('/', handlers.home.index)
 
-  app.get('/product/add', handlers.product.addGet)
-  app.post('/product/add', upload.single('image'), handlers.product.addPost)
+  app.get('/product/add', auth.isAuthenticated, handlers.product.addGet)
+  app.post('/product/add', auth.isAuthenticated, upload.single('image'), handlers.product.addPost)
 
-  app.get('/category/add', handlers.category.addGet)
-  app.post('/category/add', handlers.category.addPost)
+  app.get('/category/add', auth.isInRole('Admin'), handlers.category.addGet)
+  app.post('/category/add', auth.isInRole('Admin'), handlers.category.addPost)
 
-  app.get('/category/:category/products', handlers.category.productByCategory)
+  app.get('/category/:category/products', auth.isAuthenticated, handlers.category.productByCategory)
 
-  app.get('/product/edit/:id', handlers.product.editGet)
-  app.post('/product/edit/:id', upload.single('image'), handlers.product.editPost)
+  app.get('/product/edit/:id', auth.isAuthenticated, handlers.product.editGet)
+  app.post('/product/edit/:id', auth.isAuthenticated, upload.single('image'), handlers.product.editPost)
 
-  app.get('/product/delete/:id', handlers.product.deleteGet)
-  app.post('/product/delete/:id', handlers.product.deletePost)
+  app.get('/product/delete/:id', auth.isAuthenticated, handlers.product.deleteGet)
+  app.post('/product/delete/:id', auth.isAuthenticated, handlers.product.deletePost)
 
-  app.get('/product/buy/:id', handlers.product.buyGet)
+  app.get('/product/buy/:id', auth.isAuthenticated, handlers.product.buyGet)
+  app.get('/product/buy/:id', auth.isAuthenticated, handlers.product.buyPost)
 
   app.get('/user/register', handlers.user.registerGet)
   app.post('/user/register', handlers.user.registerPost)
@@ -27,7 +29,7 @@ module.exports = (app) => {
   app.get('/user/login', handlers.user.loginGet)
   app.post('/user/login', handlers.user.loginPost)
 
-  app.post('/user/logout', handlers.user.logout)
+  app.post('/user/logout', auth.isAuthenticated, handlers.user.logout)
 
   app.all('*', handlers.error.error)
 }
