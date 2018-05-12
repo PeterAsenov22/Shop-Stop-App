79ea3973faf683ab66612a9e74930da020a5320c
pesho <peter12345@abv.bg>
2018-05-12 20:05:10 +0300

    pagination implemented

--- controllers/home.js
+++ controllers/home.js
@@ -2,21 +2,37 @@ const Product = require('../models/Product')
 
 module.exports.index = (req, res) => {
   let queryData = req.query
+  let page = parseInt(queryData.page) || 1
+  let pageSize = 3
 
-  Product.find({buyer: null}).sort('-createdOn').populate('category').then((products) => {
-    if (queryData.query) {
-      products = products.filter(p => p.name.toString().toLowerCase().startsWith(queryData.query.toLowerCase()))
-    }
+  Product
+    .find({buyer: null})
+    .sort('-createdOn')
+    .skip((page - 1) * pageSize)
+    .limit(pageSize)
+    .populate('category')
+    .then((products) => {
+      if (queryData.query) {
+        products = products.filter(p => p.name.toString().toLowerCase().startsWith(queryData.query.toLowerCase()))
+      }
 
-    products.forEach(function (p) { p.home = true })
-    let data = {products}
+      products.forEach(function (p) { p.home = true })
+      let info = {}
 
-    if (queryData.error) {
-      data.error = queryData.error
-    } else if (queryData.success) {
-      data.success = queryData.success
-    }
+      if (queryData.error) {
+        info.error = queryData.error
+      } else if (queryData.success) {
+        info.success = queryData.success
+      }
 
-    res.render('home/index', data)
-  })
+      res.render('home/index', {
+        error: info.error,
+        success: info.success,
+        products: products,
+        hasPrevPage: page > 1,
+        hasNextPage: products.length > 0,
+        nextPage: page + 1,
+        prevPage: page - 1
+      })
+    })
 }
